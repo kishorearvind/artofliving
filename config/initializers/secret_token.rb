acf395c35073bd0b0f4cc2a84398ac61ddf3c520
kishorearvind <kisharvind@gmail.com>
2013-04-30 12:30:46 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtOfLiving::Application.config.secret_token = '89cbf5197224a2f3cf867a0804440705c80ea4b8a64a47bb33edc85fb9ebe0d63ab860e9015d819749d1f200746290fc659be21dbe2256e9788679bda4ff472a'
